023c3b915eb8465e86ed00137c1d4685cbb6f07a
Michael Vega <michaelvega8888@gmail.com>
2022-04-27 09:23:55 -0400

    Update App.js Update Todo.js Update TodoList.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,13 @@ function App() {
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
     }, [todos])
 
+    function toggleTodo(id) {
+        const newTodos = [...todos]
+        const todo = newTodos.find(todo => todo.id === id)
+        todo.complete = !todo.complete
+        setTodos(newTodos)
+    }
+
     function handleAddTodo(e) {
         const name = todoNameRef.current.value
         if (name === '') return
@@ -29,13 +36,18 @@ function App() {
         todoNameRef.current.value = null
     }
 
+    function handleClearTodo () {
+        const newTodos = todos.filter(todo => !todo.complete)
+        setTodos(newTodos)
+    }
+
     return (
         <>
-            <TodoList todos={todos}/>
+            <TodoList todos={todos} toggleTodo={toggleTodo}/>
             <input ref={todoNameRef} type = "text" />
             <button onClick={handleAddTodo}>Add Todo </button>
-            <button>Clear Completed Todos</button>
-            <div>0 Left to do</div>
+            <button onClick={handleClearTodo}>Clear Completed Todos</button>
+            <div>{todos.filter(todo => !todo.complete).length} Left to do</div>
         </>
     );
 }
